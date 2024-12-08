d2d81548d48bfde91b772064cf6d2f5b75afb5bf
Trevor Jones <jones1876@gmail.com>
2024-12-08 10:17:37 -0500

    easier to use more freepie-like vigem plugin
    add final vigem client with some additions like exposing the button states on ds4 virtual controllers
    added xbout and dsOut plugins

---
+++ FreePIE.Core.Plugins/vigem/DualShockOutputPlugin.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FreePIE.Core.Contracts;
+using FreePIE.Core.Plugins.Globals;
+using Nefarius.ViGEm.Client.Targets.DualShock4;
+
+using Nefarius.ViGEm.Client.Targets;
+
+namespace FreePIE.Core.Plugins.vigem
+{
+    [GlobalType(Type = typeof(DsOutputGlobal), IsIndexed = true)]
+    public class DualShockOutputPlugin : ViGemPluginBase
+    {
+        public override string FriendlyName { get { return "DualShockOutput"; } }
+        public override object CreateGlobal()
+        {
+            _globals = new List<VigemGlobalBase>();
+            return new GlobalIndexer<DsOutputGlobal>(CreateGlobal);
+        }
+
+        private DsOutputGlobal CreateGlobal(int index)
+        {
+            var global = new DsOutputGlobal(index, this);
+            _globals.Add(global);
+            PlugController(index);
+            return global;
+        }
+    }
+
+    [Global(Name = "dsOut")]
+    public class DsOutputGlobal : VigemGlobalBase, IDisposable
+    {
+
+        private IDualShock4Controller controller;
+        private ushort? buttons => controller?.ButtonState;
+
+        public DsOutputGlobal(int index, ViGemPluginBase plugin) : base(index)
+        {
+
+            controller = plugin.Client.CreateDualShock4Controller();
+            controller.AutoSubmitReport = false;
+
+            //var thread = new Thread(() =>
+            //{
+            //    foreach(var bytes in controller.AwaitRawOutputReport())
+            //    {
+            //        controller_FeedbackReceived(controller, new DualShock4FeedbackReceivedEventArgs(bytes[0], bytes[1], LightbarColor);
+            //    }
+            //});
+
+            controller.Connect();
+        }
+
+        internal override void Update()
+        {
+            controller.SetDPadDirection(GetDirection(_DpadFlags));
+
+            controller.SubmitReport();
+            controller.ResetReport();
+
+            _DpadFlags = 0;
+        }
+
+
+
+        public bool cross
+        {
+            get => (buttons & DualShock4Button.Cross.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Cross, value);
+        }
+
+        public bool circle
+        {
+            get => (buttons & DualShock4Button.Circle.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Circle, value);
+        }
+
+        public bool square
+        {
+            get => (buttons & DualShock4Button.Square.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Square, value);
+        }
+
+        public bool triangle
+        {
+            get => (buttons & DualShock4Button.Triangle.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Triangle, value);
+        }
+
+        public bool L2
+        {
+            get => (buttons & DualShock4Button.ShoulderLeft.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.ShoulderLeft, value);
+        }
+
+        public bool R2
+        {
+            get => (buttons & DualShock4Button.ShoulderRight.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.ShoulderRight, value);
+        }
+
+        public bool options
+        {
+            get => (buttons & DualShock4Button.Options.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Options, value);
+        }
+
+
+        public bool share
+        {
+            get => (buttons & DualShock4Button.Share.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.Share, value);
+        }
+
+
+        public bool up
+        {
+            get => _DpadFlags.HasFlag(dpadFlags.Up);
+            set => _DpadFlags = value ? _DpadFlags | dpadFlags.Up : _DpadFlags & ~dpadFlags.Up;
+        }
+
+        public bool down
+        {
+            get => _DpadFlags.HasFlag(dpadFlags.Down);
+            set => _DpadFlags = value ? _DpadFlags | dpadFlags.Down : _DpadFlags & ~dpadFlags.Down;
+
+        }
+
+        public bool left
+        {
+            get => _DpadFlags.HasFlag(dpadFlags.Left);
+            set => _DpadFlags = value ? _DpadFlags | dpadFlags.Left : _DpadFlags & ~dpadFlags.Left;
+        }
+
+        public bool right
+        {
+            get => _DpadFlags.HasFlag(dpadFlags.Right);
+            set => _DpadFlags = value ? _DpadFlags | dpadFlags.Right : _DpadFlags & ~dpadFlags.Right;
+        }
+
+        public bool leftThumb
+        {
+            get => (buttons & DualShock4Button.ThumbLeft.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.ThumbLeft, value);
+        }
+
+        public bool rightThumb
+        {
+            get => (buttons & DualShock4Button.ThumbRight.Value) != 0;
+            set => controller.SetButtonState(DualShock4Button.ThumbRight, value);
+        }
+
+        public bool PS
+        {
+            get => (controller.SpecialButtonState & DualShock4SpecialButton.Ps.Value) != 0;
+            set => controller.SetButtonState(DualShock4SpecialButton.Ps, value);
+        }
+
+        public bool touchPad
+        {
+            get => (controller.SpecialButtonState & DualShock4SpecialButton.Touchpad.Value) != 0;
+            set => controller.SetButtonState(DualShock4SpecialButton.Touchpad, value);
+        }
+
+        /// <summary>
+        /// Acceptable values range 0 - 1
+        /// </summary>
+        public double L1
+        {
+            get => controller.LeftTrigger / 255.0;
+            set
+            {
+                if (isBetween(value, 0, 1))
+                {
+                    var v = (byte)ensureMapRange(value, 0, 1, 0, 255);
+                    controller.SetSliderValue(DualShock4Slider.LeftTrigger, v);
+                }
+            }
+        }
+
+
+        public double R1
+        {
+            get => controller.RightTrigger / 255.0;
+            set => controller.SetSliderValue(DualShock4Slider.RightTrigger, (byte)ensureMapRange(value, 0, 1, 0, 255));
+        }
+
+        public double leftStickX
+        {
+            get => mapRange(controller.LeftThumbX, 0, 255, -1, 1);
+            set => controller.SetAxisValue(DualShock4Axis.LeftThumbX, (byte)ensureMapRange(value, -1, 1, 0, 255));
+        }
+
+        public double leftStickY
+        {
+            get => -mapRange(controller.LeftThumbY, 0, 255, -1, 1);
+            set => controller.SetAxisValue(DualShock4Axis.LeftThumbY, (byte)ensureMapRange(value, 1, -1, 0, 255));
+        }
+
+        public double rightStickX
+        {
+            get => mapRange(controller.RightThumbX, 0, 255, -1, 1);
+            set => controller.SetAxisValue(DualShock4Axis.RightThumbX, (byte)ensureMapRange(value, -1, 1, 0, 255));
+
+        }
+
+
+        public double rightStickY
+        {
+            get => -mapRange(controller.RightThumbY, 0, 255, -1, 1);
+            set => controller.SetAxisValue(DualShock4Axis.RightThumbY, (byte)ensureMapRange(value, 1, -1, 0, 255));
+        }
+
+
+
+
+        [Flags]
+        private enum dpadFlags
+        {
+            None = 0, Up = 1 << 0, Right = 1 << 1, Down = 1 << 2, Left = 1 << 3
+        }
+
+        private dpadFlags _DpadFlags = 0;
+
+        private static DualShock4DPadDirection GetDirection(dpadFlags flags)
+        {
+
+            DualShock4DPadDirection retval = DualShock4DPadDirection.None;
+
+            var g = new List<KeyValuePair<dpadFlags, DualShock4DPadDirection>>()
+            {
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Up,DualShock4DPadDirection.North),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Up | dpadFlags.Right,DualShock4DPadDirection.Northeast ),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Right, DualShock4DPadDirection.East ),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Right | dpadFlags.Down,DualShock4DPadDirection.Southeast),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Down, DualShock4DPadDirection.South),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Down | dpadFlags.Left, DualShock4DPadDirection.Southwest),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Left, DualShock4DPadDirection.West),
+                new KeyValuePair<dpadFlags, DualShock4DPadDirection>( dpadFlags.Up | dpadFlags.Left, DualShock4DPadDirection.Northwest)
+            };
+
+
+            if (flags != 0)
+                retval = g.Single(gg => gg.Key == flags).Value;
+
+
+            return retval;
+
+        }
+
+        internal override void Disconnect()
+        {
+            if (controller != null)
+            {
+                controller.Disconnect();
+            }
+        }
+
+        
+
+        public void Dispose()
+        {
+            if (controller != null)
+            {
+                Disconnect();
+                controller = null;
+            }
+
+        }
+
+    }
+}
---
+++ FreePIE.Core.Plugins/vigem/XboxOutputPlugin.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+
+using FreePIE.Core.Contracts;
+using FreePIE.Core.Plugins.Globals;
+
+using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.Xbox360;
+
+namespace FreePIE.Core.Plugins.vigem
+{
+    [GlobalType(Type = typeof(XboxOutputGlobal), IsIndexed = true)]
+    public class XboxOutputPlugin : ViGemPluginBase
+    {
+        public override string FriendlyName { get { return "XboxOutput"; } }
+        public override object CreateGlobal()
+        {
+            _globals = new List<VigemGlobalBase>(4);
+            return new GlobalIndexer<XboxOutputGlobal>(CreateGlobal);
+        }
+
+        private XboxOutputGlobal CreateGlobal(int index)
+        {
+            var global = new XboxOutputGlobal(index, this);
+            _globals.Add(global);
+            PlugController(index);
+            return global;
+        }
+    }
+
+    [Global(Name = "xbOut")]
+    public class XboxOutputGlobal : VigemGlobalBase, IDisposable
+    {
+        public event RumbleEvent onRumble;
+        public delegate void RumbleEvent(byte largeMotor, byte smallMotor, byte led);
+
+        private IXbox360Controller controller;
+
+        private ushort? buttons => controller?.ButtonState;
+
+
+        public XboxOutputGlobal(int index, ViGemPluginBase plugin) : base(index)
+        {
+            controller = plugin.Client.CreateXbox360Controller();
+            controller.AutoSubmitReport = false;
+            controller.FeedbackReceived += controller_FeedbackReceived;
+            controller.Connect();
+        }
+
+        
+
+        private void controller_FeedbackReceived(object sender, Xbox360FeedbackReceivedEventArgs e)
+        {
+            onRumble?.Invoke(e.LargeMotor, e.SmallMotor, e.LedNumber);            
+        }
+
+        #region Buttons
+               
+
+        public bool a
+        {
+            get => (buttons & Xbox360Button.A.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.A, value);
+        }
+
+        public bool b
+        {
+            get => (buttons & Xbox360Button.B.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.B, value);
+        }
+
+        public bool x
+        {
+            get => (buttons & Xbox360Button.X.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.X, value);
+        }
+
+        public bool y
+        {
+            get => (buttons & Xbox360Button.Y.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Y, value);
+        }
+
+        public bool leftShoulder
+        {
+            get => (buttons & Xbox360Button.LeftShoulder.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.LeftShoulder, value);
+        }
+
+        public bool rightShoulder
+        {
+            get => (buttons & Xbox360Button.RightShoulder.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.RightShoulder, value);
+        }
+
+        public bool start
+        {
+            get => (buttons & Xbox360Button.Start.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Start, value);
+        }
+
+        public bool back
+        {
+            get => (buttons & Xbox360Button.Start.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Start, value);
+        }
+
+        public bool up
+        {
+            get => (buttons & Xbox360Button.Up.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Up, value);
+        }
+
+        public bool down
+        {
+            get => (buttons & Xbox360Button.Down.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Down, value);
+        }
+
+        public bool left
+        {
+            get => (buttons & Xbox360Button.Left.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Left, value);
+        }
+
+        public bool right
+        {
+            get => (buttons & Xbox360Button.Right.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.Right, value);
+        }
+
+        public bool leftThumb
+        {
+            get => (buttons & Xbox360Button.LeftThumb.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.LeftThumb, value);
+        }
+
+        public bool rightThumb
+        {
+            get => (buttons & Xbox360Button.RightThumb.Value) != 0;
+            set => controller.SetButtonState(Xbox360Button.RightThumb, value);
+        }
+
+        /// <summary>
+        /// Acceptable values range 0 - 1
+        /// </summary>
+        public double leftTrigger
+        {
+            get => controller.LeftTrigger / 255.0;
+            set
+            {
+                if (value >= 0 && value <= 1)
+                {
+                    var v = (byte)this.ensureMapRange(value, 0, 1, 0, 255);
+                    controller.SetSliderValue(Xbox360Slider.LeftTrigger, (byte)v);
+                }
+            }
+        }
+
+        public double rightTrigger
+        {
+            get => controller.RightTrigger / 255.0;
+            set
+            {
+                if (value >= 0 && value <= 1)
+                {
+                    var v = this.ensureMapRange(value, 0, 1, 0, 255);
+                    controller.SetSliderValue(Xbox360Slider.RightTrigger, (byte)v);
+                }
+            }
+        }
+
+        public double leftStickX
+        {
+            get
+            {
+                if (controller.LeftThumbX < 0)
+                    return controller.LeftThumbX / 32768.0;
+
+                return controller.LeftThumbX / 32767.0;
+            }
+            set
+            {
+                controller.SetAxisValue(Xbox360Axis.LeftThumbX, (short)this.ensureMapRange(value, -1, 1, -32768, 32767));
+                var lx = controller.LeftThumbX;
+            }
+        }
+
+
+
+        public double leftStickY
+        {
+
+            get
+            {
+                if (controller.LeftThumbY < 0)
+                    return controller.LeftThumbY / 32768.0;
+
+                return controller.LeftThumbY / 32767.0;
+            }
+            set => controller.SetAxisValue(Xbox360Axis.LeftThumbY, (short)this.ensureMapRange(value, -1, 1, -32768, 32767));
+
+        }
+
+        public double rightStickX
+        {
+            get
+            {
+                if (controller.RightThumbX < 0)
+                    return controller.RightThumbX / 32768.0;
+
+                return controller.RightThumbX / 32767.0;
+            }
+            set => controller.SetAxisValue(Xbox360Axis.RightThumbX, (short)this.ensureMapRange(value, -1, 1, -32768, 32767));
+
+        }
+
+
+        public double rightStickY
+        {
+            get
+            {
+                if (controller.RightThumbY < 0)
+                    return controller.RightThumbY / 32768.0;
+
+                return controller.RightThumbY / 32767.0;
+            }
+            set => controller.SetAxisValue(Xbox360Axis.RightThumbY, (short)this.ensureMapRange(value, -1, 1, -32768, 32767));
+        }
+
+        #endregion
+
+
+        internal override void Disconnect()
+        {
+            if (controller != null)
+            {
+                controller.Disconnect();
+            }
+        }
+
+        internal override void Update()
+        {
+            controller.SubmitReport();
+            controller.ResetReport();
+        }
+
+        public void Dispose()
+        {
+            if (controller != null)
+            {
+                Disconnect();
+                controller = null;
+            }
+
+        }
+    }
+}
