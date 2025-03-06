2890ec589f1e7217b3ecb942159e20ee86324650
Trevor Jones <jones1876@gmail.com>
2025-03-06 18:25:36 -0500

    Add example telemetry plugin for mmf and udp and possible more in the future

---
+++ FreePIE.Core.Plugins/Telemetry/MMFPlugin.cs
@@ -0,0 +1,89 @@
+﻿using FreePIE.Core.Contracts;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FreePIE.Core.Plugins.Telemetry
+{
+    /// <summary>
+    /// this can be anything you want
+    /// </summary>
+    public struct ExampleData
+    {
+        public double X;
+        public double Y;
+        public double Z;
+        public double Yaw;
+        public double Pitch;
+        public double Roll;
+    }
+
+    [GlobalType(Type = typeof(MMFGlobal))]
+    public class MMFPlugin : Plugin
+    {
+        private MmfTelemetry<ExampleData> _telemetry;
+
+        public ExampleData Data { get; private set; }
+
+        public override string FriendlyName => "MMF";
+
+
+        bool sending = false;
+        
+        public override object CreateGlobal()
+        {
+            return new MMFGlobal(this);
+        }
+
+        public override Action Start()
+        {
+             _telemetry = new MmfTelemetry<ExampleData>(new MmfTelemetryConfig()
+            {
+                Name = "FreePIE" // this is the name of the memory mapped file
+            });
+
+            return null;
+        }
+
+        public override void Stop()
+        {
+            _telemetry.Dispose();
+        }
+
+        public override void DoBeforeNextExecute()
+        {
+            if(_telemetry != null)
+            {
+                if (sending)
+                {
+                    // this would probably be similar to the TrackIr plugin
+                }                
+                
+                Data = _telemetry.Receive();
+
+                OnUpdate();
+            }
+        }
+    }
+
+    public class MMFGlobal : UpdateblePluginGlobal<MMFPlugin>
+    {
+        
+        public MMFGlobal(MMFPlugin plugin) : base(plugin)
+        {
+        }
+
+
+        //not sure how we would make setting data generic from the global
+        public ExampleData data
+        {
+            get => plugin.Data;
+            //set => plugin.telemetry = value;
+        }
+
+    }
+}
---
+++ FreePIE.Core.Plugins/Telemetry/MmfTelemetry.cs
@@ -0,0 +1,72 @@
+﻿
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO.MemoryMappedFiles;
+
+
+using System.Runtime.InteropServices;
+
+namespace FreePIE.Core.Plugins
+{
+    internal class MmfTelemetryConfig
+    {
+        public string Name { get; set; } = "MmfTelemetry";        
+    }
+
+    internal class MmfTelemetry<TData> : TelemetryBase<TData, MmfTelemetryConfig>
+        where TData : struct
+    {
+
+        private MemoryMappedFile _mmf;
+        private MemoryMappedViewAccessor _accessor;
+        private int _dataSize = Marshal.SizeOf<TData>();
+
+        //static Mutex mutex;
+
+
+        public MmfTelemetry(MmfTelemetryConfig config) : base(config)
+        {
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
+        protected override void Configure(MmfTelemetryConfig config)
+        {
+
+            _mmf = MemoryMappedFile.CreateOrOpen(config.Name, Marshal.SizeOf<TData>());
+
+            _accessor = _mmf.CreateViewAccessor();
+
+        }
+
+
+        public override TData Receive()
+        {
+
+            TData data = default(TData);
+            _accessor.Read(0, out data);
+            return data;
+
+        }
+
+
+
+        public override int Send(TData data)
+        {
+            _accessor.Write(0, ref data);
+            return _dataSize;
+        }
+
+        public override void Dispose()
+        {
+            _mmf?.Dispose();
+            _accessor?.Dispose();
+        }
+
+
+        public override Task<TData> ReceiveAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return Task.FromResult(Receive());
+        }
+
+    }
+}
