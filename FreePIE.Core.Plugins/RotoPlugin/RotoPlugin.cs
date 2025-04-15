feecb78e86ad87f1f53e2ef623cafcd6e1855253
Trevor Jones <jones1876@gmail.com>
2025-04-15 01:00:21 -0400

    add RotoVR plugin

---
+++ FreePIE.Core.Plugins/RotoPlugin/RotoPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreePIE.Core.Plugins.RotoPlugin
+{
+    public class RotoPlugin
+    {
+    }
+
+    public class RotoPluginGlobal
+    {
+        public double yaw;
+
+        public RotoPluginGlobal()
+        {
+            
+        }
+        public void Update()
+        {
+            // Update logic for the Roto plugin
+        }
+    }
+}
